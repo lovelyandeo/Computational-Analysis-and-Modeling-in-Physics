9a3f5dc48798e1ecd891fe713064e178bef9e019
Lovely Andeo <lovelyandeo4@gmail.com>
2023-06-09 12:20:31 +0800

    edits

--- [8] ML - Perception and Logistic Regression/features.py
+++ [8] ML - Perception and Logistic Regression/features.py
@@ -6,7 +6,7 @@ import pandas as pd
 # You can edit this manually with the operations you need for the image
 from skimage.morphology import erosion, dilation, closing, opening
 
-def extract_features(img_name, lower_threshold, upper_threshold, element, operation1, operation2, properties):
+def extract_features_graphs(img_name, lower_threshold, upper_threshold, element, operation1, operation2, properties):
     I = Image.open(img_name)
     I_gray = I.convert('L')
 
@@ -48,9 +48,10 @@ def extract_features(img_name, lower_threshold, upper_threshold, element, operat
     
     return df
 
-def extract_group_features(img_name, lower_threshold, upper_threshold, element, operation1, operation2, properties):
+def extract_features_only(img_name, lower_threshold, upper_threshold, element, operation1, operation2, properties):
     I = Image.open(img_name)
     I_gray = I.convert('L')
+    I_rgb = I.convert('RGB')
 
     # Specifying the range values for the threshold
     threshold_range = (lower_threshold, upper_threshold)
@@ -60,8 +61,25 @@ def extract_group_features(img_name, lower_threshold, upper_threshold, element, 
     seg = operation2(operation1(BW, element), element)
     labeled_image = label(seg)
 
-    # Displaying the properties or features as a table
+    # Find the largest object (assuming it's the desired fruit)
+    region_props = regionprops_table(labeled_image, properties=properties)
+    largest_object_idx = np.argmax(region_props['area'])
+    largest_object_label = region_props['label'][largest_object_idx]
+
+    # Apply mask to focus on the region of the largest object
+    mask = labeled_image == largest_object_label
+    object_pixels = I_rgb.copy()
+    object_pixels[np.logical_not(mask)] = 0
+
+    # Compute the histogram of color values for the object pixels
+    hist_values = []
+    for channel in range(3):  # Iterate over RGB channels
+        hist = np.histogram(object_pixels.getchannel(channel).flatten(), bins=256, range=(0, 256))[0]
+        hist_values.extend(hist)
+
+    # Display the properties or features as a table
     df = pd.DataFrame(regionprops_table(labeled_image, properties=properties))
+    df['hist_values'] = hist_values  # Add histogram color values as a feature column
     df = df.reset_index(drop=True)
     
     return df
